649f50360e0276ec823cef0fc65e9c1fe6a9b037
Hailyn Mora <hailinmora.1897@gmail.com>
2021-02-03 12:38:49 -0500

    pie de pagina

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -14,6 +14,12 @@
         <div class = "container">
             @yield('contenido')
         </div>
+        <footer class="page-footer font-small " >
+            <div class="card text-white bg-warning mb-3" style="max-width: 90rem;">
+                <div class="card-header" align="center">Hailyn Dayana Mora Portillo</div>
+                <div class="card-header" align="center">Parcial B</div>
+            </div>
+        </footer>
         <!-- Cargar archivos Javascript -->
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
