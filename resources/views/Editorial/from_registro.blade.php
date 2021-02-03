24b61511f59f3d0e239576378321d6c3a367bdb0
Hailyn Mora <hailinmora.1897@gmail.com>
2021-02-03 13:13:44 -0500

    validacion

--- resources/views/Editorial/from_registro.blade.php
+++ resources/views/Editorial/from_registro.blade.php
@@ -7,20 +7,20 @@
         <!-- Etiquetas de tipo text con un value asignado -->
         <div class="form-group">
             <label for="nombre">Nombre </label>
-            <input type="text" id="nombre" name="nombre"  class="form-control" placeholder="Nombre" aria-label="Recipient's username" aria-describedby="basic-addon2">
+            <input type="text" id="nombre" name="nombre"  class="form-control" placeholder="Nombre" aria-label="Recipient's username" aria-describedby="basic-addon2" required>
         </div>
 
         <div class="form-group">
             <label for="direccion">Dirección </label>
-            <input type="text" id="direccion" name="direccion"  class="form-control" placeholder="Direccioón" aria-label="Recipient's username" aria-describedby="basic-addon2">
+            <input type="text" id="direccion" name="direccion"  class="form-control" placeholder="Direccioón" aria-label="Recipient's username" aria-describedby="basic-addon2" required>
         </div>
         <div class="form-group">
             <label for="ciudad">Ciudad </label>
-            <input type="text" id="ciudad" name="ciudad"  class="form-control" placeholder="Ciudad" aria-label="Recipient's username" aria-describedby="basic-addon2">
+            <input type="text" id="ciudad" name="ciudad"  class="form-control" placeholder="Ciudad" aria-label="Recipient's username" aria-describedby="basic-addon2" required>
         </div>
         <div class="form-group">
             <label for="telefono">Telefono </label>
-            <input type="text" id="telefono" name="telefono"  class="form-control" placeholder="Telefono" aria-label="Recipient's username" aria-describedby="basic-addon2">
+            <input type="text" id="telefono" name="telefono"  class="form-control" placeholder="Telefono" aria-label="Recipient's username" aria-describedby="basic-addon2"required>
         </div>
 
 
