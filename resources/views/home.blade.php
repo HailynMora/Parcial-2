777c822bdbd1fa789e38789aa41de14fbd43021f
Hailyn Mora <hailinmora.1897@gmail.com>
2021-02-03 07:51:08 -0500

    diseño

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('principal')
+@section('contenido')
+    <h1> Bienvenidos "Libreria Colombia" </h2>
+    <div>
+    <img src="https://i.pinimg.com/originals/97/57/93/975793fff4eb682dcc396c2885a13f2f.jpg" class="d-block w-100" alt="..." height ="550">
+           
+    </div>
+@stop
